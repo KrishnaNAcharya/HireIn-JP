56b2afd16d438afa0771f4c60f5feef439a8cc82
KrishnaNAcharya <knacharyakavoor@gmail.com>
2025-02-23 16:49:39 +0530

    v1

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAJuy-amWWkppId7dTwJpkZG8406aYBBYU",
-  authDomain: "hirein-jp.firebaseapp.com",
-  projectId: "hirein-jp",
-  storageBucket: "hirein-jp.appspot.com",
-  messagingSenderId: "524412551787",
-  appId: "1:524412551787:web:9ffffcb2849e8d5c9ce72e"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
